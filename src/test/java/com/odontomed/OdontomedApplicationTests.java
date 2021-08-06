93c6343fcf82c85a72a85831b9d5a027de146107
Elias Franco <eliaass95@gmail.com>
2021-08-06 10:28:28 -0300

    initial commit

---
+++ src/test/java/com/odontomed/OdontomedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
